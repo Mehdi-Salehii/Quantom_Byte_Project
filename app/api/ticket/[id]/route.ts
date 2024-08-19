11a19627f1e7d0343043d7a46f24700803a89641
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-08-20 00:52:34 +0330

    resolving production error 5

--- app/api/ticket/[id]/route.ts
+++ app/api/ticket/[id]/route.ts
@@ -6,11 +6,6 @@ export const GET = async (
   request: Request,
   route: { params: { id: string } },
 ): Promise<NextResponse<TicketType[]>> => {
-  let time = 0
-  const timer = setInterval(() => {
-    time += 1
-  }, 1)
-
   const data = await getOneTicket(route.params.id)
 
   return NextResponse.json(data)
