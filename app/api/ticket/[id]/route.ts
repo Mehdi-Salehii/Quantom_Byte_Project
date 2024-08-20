2081fe5a6a73a648d2bf967bc05dc4444e4b6058
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-08-20 14:27:05 +0330

    changed api respnsed from nextrespnse to native respnse due to prerendering isuue

--- app/api/ticket/[id]/route.ts
+++ app/api/ticket/[id]/route.ts
@@ -1,13 +1,11 @@
-import { TicketType } from "@/supabase/functions/common/schema"
 import { getOneTicket } from "@/utils/db_functions"
-import { NextResponse } from "next/server"
 
 export const GET = async (
   request: Request,
-  route: { params: { id: string } },
-): Promise<NextResponse<TicketType[] | string>> => {
-  if (process.env.NODE_ENV === "production") return NextResponse.json("data")
-  const data = await getOneTicket(route.params.id)
+  { params }: { params: { id: string } },
+) => {
+  // if (process.env.NODE_ENV === "production") return Response.json("data")
+  const data = await getOneTicket(params.id)
 
-  return NextResponse.json(data)
+  return Response.json(data)
 }
--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -1,12 +1,8 @@
-import { TicketType } from "@/supabase/functions/common/schema"
 import { getAllTickets } from "@/utils/db_functions"
-import { NextResponse } from "next/server"
 
 //TODO user authority for getting tickets hasnt approved yet after implementing clerk tickets should be filtered with user id
-export const GET = async (
-  request: Request,
-): Promise<NextResponse<TicketType[] | string>> => {
-  if (process.env.NODE_ENV === "production") return NextResponse.json("data")
+export const GET = async (request: Request) => {
+  // if (process.env.NODE_ENV === "production") return Response.json("data")
   const data = await getAllTickets()
-  return NextResponse.json(data)
+  return Response.json(data)
 }
