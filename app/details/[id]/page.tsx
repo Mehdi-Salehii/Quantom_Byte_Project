d22ef65c3a26169d5620f23c5553c853065051c5
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-01 15:59:44 +0330

    added loader and condition to details page

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -1,83 +1,122 @@
+"use client"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { tickets } from "@/utils/dummyData"
 import Image from "next/image"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import { useAuth } from "@clerk/nextjs"
+import { useState } from "react"
+import { TicketType } from "@/supabase/functions/common/schema"
+import ServerErrorOutgoing from "@/components/ServerErrorOutgoing"
+import DetailsPageLoader from "@/components/DetailsPageLoader"
 
-export default async function TicketDetails({
-  params,
-}: {
-  params: { id: string }
-}) {
-  const ticket = tickets.find((t) => t.id === params.id)
+export default function TicketDetails({ params }: { params: { id: string } }) {
+  const dummyTicket = tickets.find((t) => t.id === params.id)
+  const [data, setData] = useState<TicketType>()
+  const ticket = dummyTicket ? dummyTicket : data
+  console.log(ticket)
+  const [errorInDb, setErrorInDb] = useState(false)
+  const { userId } = useAuth()
+  const {
+    data: ticketsSent,
+    isFetching,
+    refetch,
+  } = useQuery({
+    queryKey: ["ticket-details"],
+    queryFn: async () => {
+      try {
+        const { data: userTicket } = await axios.get(`/api/ticket/${params.id}`)
 
-  if (!ticket) {
+        if (!userTicket) {
+          setErrorInDb(true)
+          return
+        }
+        if (userTicket?.length) setData(userTicket[0])
+        return data
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    enabled: !!userId && !dummyTicket,
+  })
+
+  if (!ticket && !isFetching) {
     return <p className="text-center text-red-500">Ticket not found!</p>
   }
 
   return (
     <div className="container mx-auto my-10 flex justify-center px-4">
-      <Card
-        className={`${(ticket.status === "fulfilled" && "bg-green-200/30") || (ticket.status === "rejected" && "bg-red-200/30") || (ticket.status === "processing" && "bg-purple-200/30")}`}
-      >
-        <CardHeader className="space-y-4">
-          <div className="self-center">
-            <Badge
-              variant="secondary"
-              className={`${
-                ticket.status === "fulfilled"
-                  ? "bg-green-500 text-white"
-                  : ticket.status === "rejected"
-                    ? "bg-red-500 text-white"
-                    : "bg-purple-500 text-white"
-              } grid place-items-center px-2 py-[5px]`}
-            >
-              {ticket.status}
-            </Badge>
-          </div>
-          <CardTitle>{ticket.title}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col md:flex-row md:justify-between">
-            <div className="flex-1">
-              <h3 className="text-xl font-semibold">Description</h3>
-              <p className="mt-2 text-sm text-muted-foreground">
-                {ticket.description}
-              </p>
+      {!isFetching && !errorInDb && !!ticket && (
+        <Card
+          className={`${(ticket.status === "fulfilled" && "bg-green-200/30") || (ticket.status === "rejected" && "bg-red-200/30") || (ticket.status === "processing" && "bg-purple-200/30")}`}
+        >
+          <CardHeader className="space-y-4">
+            <div className="self-center">
+              <Badge
+                variant="secondary"
+                className={`${
+                  ticket.status === "fulfilled"
+                    ? "bg-green-500 text-white"
+                    : ticket.status === "rejected"
+                      ? "bg-red-500 text-white"
+                      : "bg-purple-500 text-white"
+                } grid place-items-center px-2 py-[5px]`}
+              >
+                {ticket.status}
+              </Badge>
             </div>
-          </div>
-          {ticket.fulfill_message && (
-            <div className="mt-6">
-              <h3 className="text-xl font-semibold">
-                Your Ticket Was Fulfilled
-              </h3>
-              <p className="mt-2 text-sm text-green-600">
-                {ticket.fulfill_message}
-              </p>
+            <CardTitle>{ticket.title}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col md:flex-row md:justify-between">
+              <div className="flex-1">
+                <h3 className="text-xl font-semibold">Description</h3>
+                <p className="mt-2 text-sm text-muted-foreground">
+                  {ticket.description}
+                </p>
+              </div>
             </div>
-          )}
-          {ticket.reject_message && (
-            <div className="mt-6">
-              <h3 className="text-xl font-semibold">
-                Your Ticket Was Rejected
-              </h3>
-              <p className="mt-2 text-sm text-red-600">
-                {ticket.reject_message}
-              </p>
-            </div>
-          )}
+            {ticket.fulfill_message && (
+              <div className="mt-6">
+                <h3 className="text-xl font-semibold">
+                  Your Ticket Was Fulfilled
+                </h3>
+                <p className="mt-2 text-sm text-green-600">
+                  {ticket.fulfill_message}
+                </p>
+              </div>
+            )}
+            {ticket.reject_message && (
+              <div className="mt-6">
+                <h3 className="text-xl font-semibold">
+                  Your Ticket Was Rejected
+                </h3>
+                <p className="mt-2 text-sm text-red-600">
+                  {ticket.reject_message}
+                </p>
+              </div>
+            )}
 
-          {!!ticket?.pictures?.length && (
-            <div className="mt-5 flex w-full items-center justify-center">
-              <Image
-                src={ticket?.pictures?.[0]}
-                width={600}
-                height={600}
-                alt="ticket picture"
-              />
-            </div>
-          )}
-        </CardContent>
-      </Card>
+            {!!ticket?.pictures?.length && (
+              <div className="mt-5 flex w-full items-center justify-center">
+                <Image
+                  src={ticket?.pictures?.[0]}
+                  width={600}
+                  height={600}
+                  alt="ticket picture"
+                />
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      )}
+      {!isFetching && errorInDb && <ServerErrorOutgoing refetch={refetch} />}
+      {isFetching && (
+        <div className="grid h-full w-full place-items-center">
+          <DetailsPageLoader />
+        </div>
+      )}
     </div>
   )
 }
---
+++ components/DetailsPageLoader.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import { Skeleton } from "./ui/skeleton"
+
+export default function DetailsPageLoader() {
+  return (
+    <div className="mx-auto my-10 flex max-h-svh w-full justify-center overflow-hidden px-4 xsm:w-4/5 sm:w-1/2 md:w-[35%] lg:w-[30%]">
+      <Card className="w-full max-w-screen-md">
+        <CardHeader className="space-y-4">
+          <div className="self-center">
+            <Skeleton className="h-6 w-20" />
+          </div>
+          <CardTitle>
+            <Skeleton className="mx-auto h-8 w-3/4" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col md:flex-row md:justify-between">
+            <div className="flex-1">
+              <h3 className="text-xl font-semibold">
+                <Skeleton className="h-5 w-32" />
+              </h3>
+              <p className="mt-2">
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="mt-2 h-4 w-3/4" />
+              </p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <h3 className="text-xl font-semibold">
+              <Skeleton className="h-5 w-36" />
+            </h3>
+            <p className="mt-2">
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="mt-2 h-4 w-3/4" />
+            </p>
+          </div>
+          <div className="mt-6">
+            <h3 className="text-xl font-semibold">
+              <Skeleton className="h-5 w-40" />
+            </h3>
+            <p className="mt-2">
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="mt-2 h-4 w-3/4" />
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
