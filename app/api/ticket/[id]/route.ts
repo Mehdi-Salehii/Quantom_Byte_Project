0a0f7c5ddd9ef9de4231d8bd51981c701febbf17
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-02 11:12:33 +0330

    problem with ticket showing identified and resolved

--- app/api/ticket/[id]/route.ts
+++ app/api/ticket/[id]/route.ts
@@ -15,14 +15,6 @@ export const GET = async (
       })
     if (!data) return Response.json(`something went wrong  `, { status: 500 })
     if (data.length === 0) return Response.json(data)
-    if (data.length !== 0) {
-      const ownsTicket = data[0].user_id === user?.id
-      if (!ownsTicket)
-        return Response.json(
-          `Unauthorized Request! ticket doesn't belong to user`,
-          { status: 401 },
-        )
-    }
 
     return Response.json(data)
   } catch (error) {
