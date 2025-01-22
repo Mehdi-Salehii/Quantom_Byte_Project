db83a97c74752b9cf6435800e7498c21e01d8464
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2025-01-22 16:06:13 +0330

    debugging storage

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,13 @@ const nextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "54321",
-        pathname: "/storage/v1/object/public/quantom-byte/**",
+        pathname: "/storage/v1/object/public/media/**",
       },
       {
         protocol: "https",
         hostname: "knyyzaplbbklblrmwfim.supabase.co",
 
-        pathname: "/storage/v1/object/public/quantom-byte/**",
+        pathname: "/storage/v1/object/public/media/**",
       },
     ],
   },
