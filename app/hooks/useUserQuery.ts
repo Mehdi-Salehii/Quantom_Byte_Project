e8925b9ef900a278e3ff047b801a04345977fcd6
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2025-06-13 19:53:05 +0330

    created hook for user query

---
+++ app/hooks/useUserQuery.ts
@@ -0,0 +1,22 @@
+import { useAuth } from "@clerk/nextjs"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
+const useUserQuery = () => {
+  const { userId } = useAuth()
+  const query = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      try {
+        const { data } = await axios.get(`/api/user?id=${userId}`)
+
+        return data
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    enabled: !!userId,
+  })
+  return { ...query, userId }
+}
+export default useUserQuery
